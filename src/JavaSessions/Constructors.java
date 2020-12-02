6942c92aa576d3e1a81f2854cdcfb8c37f58444a
Srinn <49578943+SrinnVass@users.noreply.github.com>
2020-12-02 13:10:32 +0530

    Update Constructors.java

--- src/JavaSessions/Constructors.java
+++ src/JavaSessions/Constructors.java
@@ -35,11 +35,11 @@ public class Constructors {
 		Constructors c3 = new Constructors("laxmi", 103, "admin");
 		  
 		
-		System.out.println(c1.name + c1.rollno +c1.dept);
+		System.out.println(c1.name + ":::::" + c1.rollno + ":::::" + c1.dept);
 		
-		System.out.println(c2.name + c2.rollno + c2.dept );
+		System.out.println(c2.name + ":::::" + c2.rollno + ":::::" + c2.dept );
 		
-		System.out.println(c3.name + c3.rollno + c3.dept);
+		System.out.println(c3.name + ":::::" + c3.rollno + ":::::" + c3.dept);
 		
 	}
 
